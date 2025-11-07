590f04389c150fd1a8357444f568acb4880cfc8a
Guillermo Munoz <gumunoz@c1r14s2.42madrid.com>
2025-11-07 19:06:08 +0100

    modular code

--- pipex.c
+++ pipex.c
@@ -1,20 +1,16 @@
-#include "PIPEX_H"
-# include <unistd.h>
-# include <errno.h>
-# include <fcntl.h>
+#include "pipex.h"
 
 char	*found_path(char **envp,char **cmd1_parsed)
 {
 	char	*ruta_prueba;
 	char	**path;
-	int i;
+	int 	i;
 	char	*temp;
 
 	i = 0;
 	while(!ft_strnstr(envp[i], "PATH=", 5))
 		i++;
 	path = ft_split(ft_strchr(envp[i],'=') + 1, ':');
-	
 	i = 0;
 	temp = ft_strjoin("/",cmd1_parsed[0]);
 	while(path[i] != NULL)
@@ -26,29 +22,16 @@ char	*found_path(char **envp,char **cmd1_parsed)
 			free(ruta_prueba);
 		}
 		else
-		{
-			free(temp);
-			path = NULL;
-			free(path);
-			return (ruta_prueba);
-		}
+			return (free(temp), free(path), ruta_prueba);
 	}
-	free(temp);
-	free(path);
-	return (NULL);
+	return (free(temp), free(path), NULL);
 }
 
 void	pipex(char *infile, char *cmd1, char *cmd2, char *outfile, char **envp)
 {
-	int		i;
 	int		id;
 	int		fd[2];
-	int		fd_in;
-	int		fd_out;
-	char	**cmd1_parsed;
-	char	**cmd2_parsed;
-	char	*path;
-	i = 0;
+	
 	if (pipe(fd) == -1)
 	{
 		perror ("pipe");
@@ -61,39 +44,8 @@ void	pipex(char *infile, char *cmd1, char *cmd2, char *outfile, char **envp)
 		exit (1);
 	}
 	else if (id == 0)
-	{
-		//hijo
-		fd_in = open(infile, O_RDONLY);
-		if (fd_in == -1)
-		{
-			perror("problem whit first fd");
-			exit (1);
-		}
-		//redirigir a infile y out y cerrar los que ya no usamos
-		if (dup2(fd_in, 0) == -1)
-			perror("dup2");
-		close(fd_in);
-		close(fd[0]);
-		if (dup2(fd[1], 1) == -1)
-			perror("dup2");
-		close(fd[1]);
-		
-		//parsear con split para poder usar execve
-		cmd1_parsed = ft_split(cmd1,' ');
-		//sacar path de envp
-		path = found_path(envp, cmd1_parsed);
-		if (!path)
-			{
-				perror("command not found");
-				exit (127);
-			}
-		execve(path, cmd1_parsed, envp);
-		perror("command not found");
-		exit(127);
-		
-	}
+		create_first_child(infile, fd, cmd1, envp);
 	else
-	//este es el padre
 	{
 		id = fork();
 		if (id == -1)
@@ -102,31 +54,83 @@ void	pipex(char *infile, char *cmd1, char *cmd2, char *outfile, char **envp)
 			exit (1);
 		}
 		else if (id == 0)
-		{
-			// creamos hijo2
-			if (dup2(fd[0], 0) == -1)
-				perror("dup2");
-			fd_out = open(outfile, O_CREAT | O_TRUNC | O_WRONLY, 0644);
-			if (dup2(fd_out, 1) == -1)
-				perror("dup2");
-			close(fd[0]);
-			close(fd[1]);
-			close (fd_out);
-			cmd2_parsed = ft_split(cmd2, ' ');
-			path = found_path(envp, cmd2_parsed);
-			if (!path)
-			{
-				perror("command not found");
-				exit (127);
-			}
-			execve(path, cmd2_parsed, envp);
-			perror("command not found");
-			exit (127);
-
-		}
+			create_second_child(outfile, fd, cmd2, envp);
 		close(fd[0]);
 		close(fd[1]);
 		wait(NULL);
 		wait(NULL);
 	}
+}
+
+void	create_second_child(char *outfile, int fd[2], char *cmd2, char **envp)
+{
+	int		fd_out;
+	char	**cmd2_parsed;
+	char	*path;
+
+	if (dup2(fd[0], 0) == -1)
+				perror("dup2");
+	fd_out = open(outfile, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (dup2(fd_out, 1) == -1)
+		perror("dup2");
+	close(fd[0]);
+	close(fd[1]);
+	close (fd_out);
+	cmd2_parsed = ft_split(cmd2, ' ');
+	path = found_path(envp, cmd2_parsed);
+	if (!path)
+	{
+		perror("command not found");
+		exit (127);
+	}
+	execve(path, cmd2_parsed, envp);
+	perror("command not found");
+	exit (127);
+}
+void	create_first_child(char *infile, int fd[2], char *cmd1, char **envp)
+{
+	int		fd_in;
+	char	**cmd1_parsed;
+	char	*path;
+
+	fd_in = open(infile, O_RDONLY);
+	if (fd_in == -1)
+	{
+		perror("problem with first fd");
+		exit (1);
+	}
+	redirection_close(fd_in, fd);
+	cmd1_parsed = ft_split(cmd1,' '); //parsear con split para poder usar execve
+	path = found_path(envp, cmd1_parsed);//sacar path de envp
+	if (!path)
+		{
+			perror("command not found");
+			exit (127);
+		}
+	execve(path, cmd1_parsed, envp);
+	perror("command not found");
+	exit(127);
+}
+void	redirection_close(int fd_in, int fd[2])
+{
+	if (dup2(fd_in, 0) == -1)//redirigir a infile y out y cerrar los que ya no usamos
+		perror("dup2");
+	close(fd_in);
+	close(fd[0]);
+	if (dup2(fd[1], 1) == -1)
+		perror("dup2");
+	close(fd[1]);
+	return ;
+}
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc != 5)
+	{
+		ft_putstr_fd("Usage: ./pipex infile cmd1 cmd2 outfile\n", 2);
+		return (1);
+	}
+	pipex(argv[1],argv[2],argv[3], argv[4], envp);
+	return (0);
 }
